c9c7c1a42992bdb119982708cd6f22cfa3ad8e96
medotnull <meenalbansodbmw@gmail.com>
2026-01-05 23:58:02 +0530

    empty routes

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const app = express();
+
+app.post("/api/v1/signup", async (req, res) => {
+
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+
+})
+
+app.post("/api/v1/content", async (req, res) => {
+
+})
+
+app.get("/api/v1/content", async (req, res) => {
+
+})
